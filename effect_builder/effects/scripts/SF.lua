8f3298c0abb496dfcbc6f7ed085e86445713e151
Wil Thieme <wil.thieme@protonmail.com>
2022-08-13 08:59:44 -0400

    initial effect builder support

---
+++ effect_builder/effects/scripts/SF.lua
@@ -0,0 +1,5 @@
+
+-- luacheck: globals createEffectString parentcontrol number_value
+function createEffectString()
+    return parentcontrol.window.effect.getStringValue() .. ": " .. number_value.getStringValue()
+end
