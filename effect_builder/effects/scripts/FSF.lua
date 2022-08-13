d25d7a38634cf7c3858b63def22c72be74f40fa3
Wil Thieme <wil.thieme@protonmail.com>
2022-08-13 12:28:03 -0400

    add FSF/NSF to effect builder

---
+++ effect_builder/effects/scripts/FSF.lua
@@ -0,0 +1,9 @@
+
+-- luacheck: globals createEffectString enable_disable
+function createEffectString()
+    local sEffect = 'FSF'
+    if enable_disable.getStringValue() == 'disabled' then
+        sEffect = 'NSF'
+    end
+    return sEffect
+end
